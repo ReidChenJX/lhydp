733c88f90dee9838375aeccd56c8dca04da59265
ReidChenJX <327817014@qq.com>
2021-04-01 15:48:31 +0800

    2021-4-1 RNN

--- RecurrentNN.py
+++ RecurrentNN.py
@@ -24,16 +24,16 @@ def load_training_data(path):
     if 'training_label' in path:
         with open(path, 'r', encoding='utf-8') as f:
             lines = f.readlines()
-            lines = [line.strip('\n').replace(' \' ','\'').split() for line in lines]
-        
+            lines = [line.strip('\n').replace(' \' ', '\'').split() for line in lines]
+            
             x = [line[2:] for line in lines]
             y = [line[0] for line in lines]
-
+        
         return x, y
     else:
         with open(path, 'r', encoding='utf-8') as f:
             lines = f.readlines()
-            x = [line.strip('\n').replace(' \' ','\'').split()  for line in lines]
+            x = [line.strip('\n').replace(' \' ', '\'').split() for line in lines]
         
         return x
 
@@ -41,7 +41,7 @@ def load_training_data(path):
 def load_testing_data(path):
     with open(path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
-        X = [",".join(line.strip('\n').replace(' \' ','\'').split(',')[1:]).strip() for line in lines]
+        X = [",".join(line.strip('\n').replace(' \' ', '\'').split(',')[1:]).strip() for line in lines]
     return X
 
 
@@ -63,6 +63,7 @@ def train_word2vec(x):
                               workers=4, iter=1, sg=1)
     return model
 
+
 #
 # # 词频矩阵 word2vec 模型训练
 # if __name__ == '__main__':
@@ -77,7 +78,6 @@ def train_word2vec(x):
 #
 #     print("saving model ...")
 #     model.save(os.path.join(path_prefix, 'model/w2v_all.model'))
-
 
 
 class Preprocess:
@@ -212,29 +212,154 @@ class LSTM_Net(nn.Module):
         return x
 
 
-def training(batch_size, n_epoch, lr, model_dir, train, valid, model, device):
+def training(batch_size, n_epoch, lr, train, valid, train_no_label_data, model, device, X_train, y_train):
+    global train_loader
     total = sum(p.numel() for p in model.parameters())
     trainable = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('\nstart training, parameter total: {}, trainable:{} \n'.format(total, trainable))
     
+    loss = nn.BCELoss()  # 二元交叉熵损失 binary cross entropy
+    t_batch = len(train)
+    v_batch = len(valid)
+    
+    optimizer = optim.Adam(model.parameters(), lr=lr)  # 定义优化方式为Adam
+    total_loss, total_acc, best_acc = 0, 0, 0
+    
+    epoch_flag = 4  # 将无标签数据加入训练数据的节点
+    
+    for epoch in range(n_epoch):
+        total_loss, total_acc = 0, 0
+        
+        if epoch <= epoch_flag:
+            # 前几轮训练模型
+            model.train()
+            for i, (inputs, labels) in enumerate(train):
+                inputs = inputs.to(device, dtype=torch.long)
+                labels = labels.to(device, dtype=torch.float)
+                
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                outputs = outputs.squeeze()
+                batch_loss = loss(outputs, labels)
+                batch_loss.backward()
+                optimizer.step()
+                
+                accuracy = evaluation(outputs, labels)
+                total_acc += (accuracy / batch_size)
+                total_loss += batch_loss.item()
+            
+            print('Epoch | {}/{}'.format(epoch + 1, n_epoch))
+            print('Train | Loss:{:.5f} Acc:{:.3f}'.format(total_loss / t_batch, total_acc / t_batch * 100))
+        
+        
+        
+        if epoch == epoch_flag:
+            model.eval()
+            # self_training，模型预测无标签数据，并选择表现好的数据加入训练集
+            with torch.no_grad():
+                for i, (inputs) in enumerate(train_no_label_data):
+                    inputs = inputs.to(device, dtype=torch.long)
+                    outputs = model(inputs)
+                    outputs = outputs.squeeze()
+                    labels, id = add_label(outputs)
+                    t_batch += len(id)
+                    
+                    # 加入新的数据
+                    X_train = torch.cat((X_train.to(device), inputs[id]), dim=0)
+                    y_train = torch.cat((y_train.to(device), labels[id]), dim=0)
+            
+            # 重新定义train_data
+            train_dataset = TwitterDataset(X_train, y_train)
+            train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
+                                                       shuffle=True, num_workers=4)
+        
+        if epoch > epoch_flag:
+            model.train()
+            for i, (inputs, labels) in enumerate(train_loader):
+                inputs = inputs.to(device, dtype=torch.long)
+                labels = labels.to(device, dtype=torch.float)
+                
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                outputs = outputs.squeeze()
+                batch_loss = loss(outputs, labels)
+                batch_loss.backward()
+                optimizer.step()
+                
+                accuracy = evaluation(outputs, labels)
+                total_acc += (accuracy / batch_size)
+                total_loss += batch_loss.item()
+            
+            print('Epoch | {}/{}'.format(epoch + 1, n_epoch))
+            print('Train | Loss:{:.5f} Acc:{:.3f}'.format(total_loss / t_batch, total_acc / t_batch * 100))
+            
+            # validation
+            model.eval()
+            with torch.no_grad():
+                total_loss, total_acc = 0, 0
+                
+                for i, (inputs, labels) in enumerate(valid):
+                    inputs = inputs.to(device, dtype=torch.long)
+                    labels = labels.to(device, dtype=torch.float)
+                    
+                    outputs = model(inputs)
+                    outputs = outputs.squeeze()
+                    batch_loss = loss(outputs, labels)
+                    accuracy = evaluation(outputs, labels)
+                    total_acc += (accuracy / batch_size)
+                    total_loss += batch_loss.item()
+                
+                print("Valid | Loss:{:.5f} Acc: {:.3f} ".format(total_loss / v_batch, total_acc / v_batch * 100))
+                if total_acc > best_acc:
+                    # 如果validation 的结构比之前优，则保存模型
+                    best_acc = total_acc
+                    torch.save(model, 'ckpt.model')
+            print('-----------------------------------------------')
 
-if __name__ == '__main__':
+
+def testing(test_loader, model, device):
+    model.eval()
+    ret_output = []
+    with torch.no_grad():
+        for i, inputs in enumerate(test_loader):
+            inputs = inputs.to(device, dtype=torch.long)
+            outputs = model(inputs)
+            outputs = outputs.squeeze()
+            outputs[outputs >= 0.5] = 1
+            outputs[outputs < 0.5] = 0
+            ret_output += outputs.int().tolist()
+    
+    return ret_output
+
+
+def add_label(outputs, threshold=0.8):
+    id = (outputs >= threshold) | (outputs < 1 - threshold)
+    outputs[outputs >= threshold] = 1
+    outputs[outputs < threshold] = 0
+    return outputs.long(), id
+
+
+"""定义训练与测试的主函数"""
+
+
+def train_main():
     # # main
     device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
     
     # 处理data
     train_with_label = './hw4/training_label.txt'
     train_no_label = './hw4/training_nolabel.txt'
-    testing_data = './hw4/testing_data.txt'
     
     w2v_path = './model/w2v_all.model'
     
     sen_len = 20
     fix_embedding = True
     batch_size = 128
-    epoch = 5
+    epoch = 10
     lr = 0.001
     
     train_x, y = load_training_data(train_with_label)
+    # 优化2：训练后加入无标签数据
     train_x_no_label = load_training_data(train_no_label)
     
     # 数据预处理
@@ -256,6 +381,53 @@ if __name__ == '__main__':
     
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
                                                shuffle=True, num_workers=4)
-    val_loader = torch.utils.data.DataLoader(dataset=X_val, batch_size=batch_size, shuffle=False, num_workers=4)
+    val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size,
+                                             shuffle=False, num_workers=4)
     
+    # 无标签训练数据，数据预处理
+    preprocess = Preprocess(train_x_no_label, sen_len)
+    embedding = preprocess.make_embedding(load=True)
+    train_no_label_em = preprocess.sentence_word2idx()
+    
+    train_x_no_dataset = TwitterDataset(train_no_label_em, None)
+    
+    train_no_label_data = torch.utils.data.DataLoader(dataset=train_x_no_dataset, batch_size=batch_size,
+                                                 shuffle=False, num_workers=4)
+    # DataLoader 的 batch_size，DataLoader后数据为可迭代对象，batch_size确定每次迭代的数据量
+    
+    training(batch_size, epoch, lr, train_loader, val_loader, train_no_label_data, model, device, X_train, y_train)
 
+
+def test_main():
+    device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+    
+    sen_len = 20
+    fix_embedding = True
+    batch_size = 128
+    epoch = 10
+    lr = 0.001
+    
+    print('Loading testing data ....')
+    test_x = load_testing_data('./hw4/testing_data.txt')
+    
+    # 数据预处理
+    preprocess = Preprocess(test_x, sen_len=sen_len)
+    embedding = preprocess.make_embedding(load=True)
+    test_x = preprocess.sentence_word2idx()
+    test_dataset = TwitterDataset(test_x, None)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                              shuffle=False, num_workers=4)
+    
+    print('\nload model ...')
+    model = torch.load('ckpt.model')
+    
+    outputs = testing(test_loader, model, device)
+    
+    tmp = pd.DataFrame({"id": [str(i) for i in range(len(test_x))], "label": outputs})
+    print("save csv ...")
+    tmp.to_csv('./hw4/predict.csv', index=False)
+    print("Finished")
+
+
+if __name__ == '__main__':
+    train_main()
